afdd1c74192e50558606bbf6a444e1592155a79a
BlueCannonBall <mmttiizz100@gmail.com>
2022-09-30 13:45:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -208,6 +208,8 @@ void init_conn(pn::SharedSock<pw::Connection> conn) {
         });
         route(std::move(proxy), std::move(conn));
     } else {
+        conn->shutdown(PN_SD_RECEIVE);
+        
         size_t protocol_len;
         if (boost::starts_with(req.target, "http://")) {
             protocol_len = 7;
@@ -280,6 +282,7 @@ void init_conn(pn::SharedSock<pw::Connection> conn) {
                 ERR_WEB;
             return;
         }
+        proxy->shutdown(PN_SD_SEND);
 
         INFO("Routing HTTP request to " << split_host[0] << ':' << split_host[1]);
         route(std::move(proxy), conn);
