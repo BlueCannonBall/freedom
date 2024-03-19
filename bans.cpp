f67822a66e462aa0b3503d11246145d2c10c12bd
BlueCannonBall <mmttiizz100@gmail.com>
2024-03-18 20:21:55 -0400

    Add mutex

--- bans.cpp
+++ bans.cpp
@@ -1,7 +1,11 @@
 #include "bans.hpp"
 #include <fstream>
+#include <mutex>
+
+std::mutex mutex;
 
 std::set<std::string> get_bans() {
+    std::lock_guard<std::mutex> lock(mutex);
     std::set<std::string> ret;
     std::ifstream file("bans.txt");
     if (file.is_open()) {
@@ -11,6 +15,7 @@ std::set<std::string> get_bans() {
 }
 
 void set_bans(const std::set<std::string>& bans) {
+    std::lock_guard<std::mutex> lock(mutex);
     std::ofstream file("bans.txt");
     for (const auto& username : bans) {
         file << username << '\n';
