d61b63ee08d1ae0b8a3ced2c4830eb63af696cff
BlueCannonBall <mmttiizz100@gmail.com>
2024-03-24 12:07:48 -0400

    Fix

--- bans.hpp
+++ bans.hpp
@@ -6,7 +6,6 @@
 namespace bans {
     void init();
     std::vector<std::string> get_all_bans();
-
     void ban(const std::string& username);
     void unban(const std::string& username);
     bool is_banned(const std::string& username);
